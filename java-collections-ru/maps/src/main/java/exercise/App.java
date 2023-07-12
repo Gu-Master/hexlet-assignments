1bfda391650f0aa5c1918f03862969a7c684ed88
@hexlet/cli <support@hexlet.io>
2023-07-12 21:37:17 +0700

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,38 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+
+import java.util.*;
+
+public class App {
+
+    public static Map getWordCount(String sentens) {
+        String[] strArr = sentens.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        for (String word: strArr) {
+            map.put(word, Collections.frequency(Arrays.asList(strArr), word));
+        }
+        return map;
+
+    }
+    public static String toString(Map<String, Integer> map) {
+        StringBuilder str = new StringBuilder();
+        str.append("{\n");
+        Set<String> setKey = map.keySet();
+        for (String key : setKey) {
+            str.append("  ").append(key).append(": ").append(map.get(key)).append("\n");
+        }
+        str.append("}");
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(App.toString(App.getWordCount("i love dog i dog ")));
+
+    }
+
+}
+//END
